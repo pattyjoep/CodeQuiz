3c55918649e679a41c95ee00feff2c9584e68d75
Trick <petropatrickj@gmail.com>
2019-12-21 19:46:50 -0500

    pushing updates for scripts

--- javascript/code_quiz_questions.js
+++ javascript/code_quiz_questions.js
@@ -1,44 +1,32 @@
 //Establishing quiz questions 
-    var questionOne = {"title" : "A variable which contains (or resolves to) a value of true/false are known as ____", 
-
-    "A" : "A: strings",
-    "B" : "B: objects",
-    "C" : "C: booleans",
-    "D" : "D: local storage",
+ 
+var questions = [
+    {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+    },
     
-    "Answer" : "C: booleans"};
-       
-        // {
-        //     title: "Commonly used data types DO NOT include:",
-        //     choices: ["strings", "booleans", "alerts", "numbers"],
-        //     answer: "alerts"
-        //     },
+    {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+    },
+    
+    {
+    title : "Arrays in JavaScript can be used to store ____", 
+    choices: ["numbers and strings","other arrays", "booleans", "all of the above"],
+    answer : "all of the above"  
+    },
 
-        // {
-        //     title: "The condition in an if / else statement is enclosed within ____.",
-        //     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        //     answer: "parentheses"
-        //     },
+    {
+    title : "The block of code to be executed when an 'if' statement resolves to false is contained after ____?", 
+    choices: ["while", "else", "over", "other",],
+    answer: "else"
+    },
 
-        // {
-        //     title: "The block of code to be executed when an 'if' statement resolves to false is contained after ____?",
-        //     choices: ["while", "else", "over", "other"],
-        //     answer: "else"
-        //     },
-        
-        // {
-        //     title: "arrays in JavaScript can be used to store:",
-        //     choices: ["numbers & strings", "arrays", "booleans", "all of the above"],
-        //     answer: "all of the above"
-        //     },
+];
 
-    //     {
-    //         title: "A variable which contains (or resolves to) a value of true/false are known as ____",
-    //         choices: ["strings", "objects", "booleans", "local storage"],
-    //         answer: "booleans"
-    //         },
-
-    // ];
 
     
   
--- javascript/code_quiz_scripts.js
+++ javascript/code_quiz_scripts.js
@@ -1,34 +1,70 @@
 //Variables
     var Start = document.querySelector("#Startbtn")
+    var Submit = document.querySelector("#Submitbtn")
+
+    var StartCardDiv = document.querySelector("#StartCardDiv")
+    var QuestionsDiv = document.querySelector("#QuestionsDiv")
+    var QuestionsContainer = document.querySelector("#QuestionsContainer")
+
+    var sec = 75
+
+    var Score = 0
 
 
 //Objects
     // "javascript\code_quiz_questions.js"
 
 //Event Listeners
-    Start.addEventListener("click", StartQuiz)
-    Start.addEventListener("click", TimerCountdown)
+    Start.addEventListener("click", function(){
+        StartQuiz()
+        TimerCountdown()
+    });
+    
+   
+    
 
 //Functions 
     function StartQuiz() {
-        var StartCardDiv = document.querySelector("#StartCardDiv")
-
-        var QuestionsDiv = document.querySelector("#QuestionsDiv")
-
-        var QuestionsContainer = document.querySelector("#QuestionsContainer")
-
 
         StartCardDiv.setAttribute("class", "StartCardDivHide")
 
         QuestionsDiv.setAttribute("class", "card text-center customCard QuestionsDivShow questions-Container")
-        
-        QuestionsContainer.textContent = JSON.stringify(questions)
+
+
+        ShowQuestion(0)
+
 
     }
 
+    function ShowQuestion(i) {
+        
+        QuestionsContainer.textContent = questions[i].title
+
+        var options = questions[i].choices
+
+        for (var c = 0; c < options.length; c++) {
+            var newBtn = document.createElement("button")
+            newBtn.setAttribute("class", "customBtn")
+            newBtn.setAttribute("value", options[c])
+
+            var Break = document.createElement("br")
+
+            QuestionsContainer.appendChild(newBtn)
+            QuestionsContainer.appendChild(Break)
+          
+
+            newBtn.textContent = options[c]
+
+            if (newBtn.value === "numbers"){
+                alert("Correct!")
+            }
+        }   
+    }
+
+
     //Timer countdown
     function TimerCountdown() {
-        var sec = 75;
+
         var timer = setInterval(function(){
             document.querySelector('#timerSpan').textContent = "Time: " + sec;
             sec--;
